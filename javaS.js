bdcebe76d38b1d5ea4e2696d9eb046d73f72a532
Abdiaziz <145577421+0paziz@users.noreply.github.com>
2025-01-20 18:28:14 +0300

    initial commit

---
+++ javaS.js
@@ -0,0 +1,28 @@
+const Menu_triggerID= document.getElementById("menu");
+const Cence_triggerlID=document.getElementById("cencel");
+
+const menu_container=document.getElementById("nav-container");
+
+
+function display(){
+
+   menu_container.style.display="block"
+
+}
+
+function close(){
+
+     menu_container.style.display="none"
+}
+
+Menu_triggerID.addEventListener("click" , (event)=>{
+    event.preventDefault;
+
+    display();
+});
+
+Cence_triggerlID.addEventListener("click", ()=>{
+    close();
+})
+
+
